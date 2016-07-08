59b49ba48a1d2bf0b85f71ff767136ea8324de84
Matthew Corner <mcorner@signiant.com>
2016-07-08 10:37:00 -0400

    Added support for attachments

--- mail.py
+++ mail.py
@@ -3,6 +3,7 @@ __author__ = 'Ludmal.DESILVA'
 import os, email, smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 path = os.path.dirname(__file__)
 #modify this to change the Template Directory
@@ -27,13 +28,17 @@ class EmailTemplate():
 class MailMessage(object):
     html = False
 
-    def __init__(self, from_email='', to_emails=[], cc_emails=[], subject='', body='', template=None):
+    def __init__(self, from_email='', to_emails=[], cc_emails=[], subject='', body='', template=None, attachments=[]):
         self.from_email = from_email
         self.to_emails = to_emails
         self.cc_emails = cc_emails
         self.subject = subject
         self.template = template
         self.body = body
+        self.file_attachments = attachments
+
+    def attach_file(self, path):
+        self.file_attachments.append(path)
 
     def get_message(self):
         if isinstance(self.to_emails, str):
@@ -62,6 +67,13 @@ class MailMessage(object):
                 msg.attach(MIMEText(self.template.render(),'plain'))
         else:
                 msg.attach(MIMEText(self.body, 'plain'))
+
+        for attachment in self.file_attachments:
+            with open(attachment, "rb") as f:
+                filename = os.path.basename(attachment)
+                part = MIMEApplication(f.read(), Name=filename)
+                part['Content-Disposition'] = 'attachment; filename="' + str(filename) + '"'
+                msg.attach(part)
         return msg
 
 
